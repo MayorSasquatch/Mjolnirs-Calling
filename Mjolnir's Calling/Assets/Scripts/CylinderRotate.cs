aa2485ecf7bf255fa997f6753de6b2ce2a033b1d
Jordan R. <jordan.ritter.da@gmail.com>
2014-06-05 17:23:08 -0400

    First Build
    New Build

---
+++ Mjolnir's Calling/Assets/Scripts/CylinderRotate.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CylinderRotate : MonoBehaviour {
+
+	public float startSpeed;
+	public float speedCap;
+	public float speedIncreaseRate;
+	float curSpeed;
+	float rotation;
+
+	void Start () {
+		curSpeed = startSpeed;
+		speedIncreaseRate = speedIncreaseRate/1000;
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if(curSpeed < speedCap) curSpeed += speedIncreaseRate; // Increase speed if speed cap has not been reached
+
+		transform.Rotate(curSpeed/10,0,0,Space.World);	// Rotate the cylinder
+	}
+}
