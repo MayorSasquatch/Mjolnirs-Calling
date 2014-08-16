dd8beb8346061c4e01b68a82e81e9ec85a442b3b
David Kops <kopsdavid@hotmail.com>
2014-08-16 19:25:28 -0400

    Spawning Rates Adjusted
    - Adjusted the spawning rates for all of the objects.
    - Power up spawn percentages change as wave number increases.

--- Mjolnir's Calling/Assets/Scripts/ObjectSpawn.cs
+++ Mjolnir's Calling/Assets/Scripts/ObjectSpawn.cs
@@ -11,7 +11,7 @@ public class ObjectSpawn : MonoBehaviour {
 	float[] zCoord = {0, 60*Mathf.Cos(300*Mathf.Deg2Rad), 60*Mathf.Cos(330*Mathf.Deg2Rad)};
 	float[] angle = {180, 150, 120};
 
-	int numObj = 3;	//Number of objects to spawn per group/wave
+	int numObj = 1;	//Number of objects to spawn per group/wave
 	int waveCount; //Number of waves survived
 	bool powerUpSpawned; //Boolean to track whether a power up has been spawned this wave
 
@@ -41,6 +41,7 @@ public class ObjectSpawn : MonoBehaviour {
 		//Determine if it is time to increment the wave counter
 		if(degUntilNextWave <= 0){
 			waveCount++;
+			waveAdjust(waveCount);
 			degUntilNextWave += 180;
 		}
 		Debug.Log(waveCount);
@@ -72,35 +73,30 @@ public class ObjectSpawn : MonoBehaviour {
 	}
 
 	//Probability array locations: {tree, rock, crystal spire, Health power up, Lightning power up, Loki power up}
-	float[] objProbs = {80, 0, 0, 10, 5, 5}; //Object spawn probabilities
+	float[] objProbs = {27, 27, 27, 9, 5, 5}; //Object spawn probabilities
 	float[] cumProbs = new float[6]; //Cumulative object spawn probabilities
 
 	//Function to adjust spawn probabilities depending on wave number
 	void waveAdjust (int waveNum) {
 
-		if(waveNum <= 20) updateProbs(-1, 1, 0, 0, 0, 0);	// 60, 20, 0, 10, 5, 5
+		if(waveNum == 10) numObj++;
+		else if(waveNum == 20) numObj++;
 
-		else if(waveNum <= 40) updateProbs(-1.5f, 0.5f, 1, 0, 0, 0);	// 30, 30, 20, 10, 5, 5
+		if(waveNum > 20 && waveNum <= 60) updateProbs(-0.1f, 0, 0.1f);
 
-		else if (waveNum <= 60) updateProbs(-1, 0.75f, 0.25f, 0, 0, 0);	// 10, 45, 25, 10, 5, 5
-
-		else if (waveNum <= 95) updateProbs(0, -1, 1, 10, 5, 5);	// 10, 10, 60, 10, 5, 5
-
-		else if (waveNum <= 100) updateProbs(0, 0, 0, -1, -0.5f, 1.5f); // 10, 10, 60, 5, 2.5, 12.5
+		else if(waveNum <= 70) updateProbs(0, -0.1f, 0.1f);
+		
 	}
 
 	//Function to update the object spawn probabilities
-	void updateProbs (float tree, float rock, float crystal, float pUHealth, float pULight, float pULoki) {
-
-		objProbs[0] += tree;
-		objProbs[1] += rock;
-		objProbs[2] += crystal;
+	void updateProbs (float pUHealth, float pULight, float pULoki) {
+		
 		objProbs[3] += pUHealth;
 		objProbs[4] += pULight;
 		objProbs[5] += pULoki;
 
 		//Update cumulative object spawn probabilities
-		for(int i=0; i<6; i++){
+		for(int i=3; i<6; i++){
 			cumProbs[i] = objProbs[i];
 
 			for(int j=i-1; j>=0; j--)
@@ -114,7 +110,7 @@ public class ObjectSpawn : MonoBehaviour {
 		//If a power up has not been spawned, include its cumulative prob in the range. If one has been spawned, exclude it from the range
 		float prob;
 		if(!powerUpSpawned) prob = Random.Range(0, 100);
-		else prob = Random.Range(0, 80);
+		else prob = Random.Range(0, 81);
 
 		for(int i=0; i<6; i++){
 			if(prob <= cumProbs[i]) return i;
